7dab9b6011b9d57820673587937604d7e0c69c84
seanchang2468 <90725371+seanchang2468@users.noreply.github.com>
2023-11-14 16:44:20 -0600

    Updated pix2pix.py
    Should be fully functional

--- WebScrape/pix2pix.py
+++ WebScrape/pix2pix.py
@@ -3,13 +3,31 @@
 """
 Created on Tue Nov 14 14:46:11 2023
 
-@author: seanchang
+code taken from https://colab.research.google.com/github/tensorflow/docs/blob/master/site/en/tutorials/generative/pix2pix.ipynb#scrollTo=kz80bY3aQ1VZ
+edited by Sean Chang
+see link for more details
+
+training imgs must be held in train/ folder
+testing imgs must be held in either test/ or val/
+img file format should be *.jpeg or whatever you want the file extension to be (make sure to change IMG_EXTENSION if not jpeg)
+
+To save logs in Tenserboard: 
+%load_ext tensorboard
+%tensorboard --logdir {log_dir}
+
+checkpoint shenanigans - idk what this does or where it goes but it seems important?
+!ls {checkpoint_dir}
 """
+# Number of images in training set
+BUFFER_SIZE = 1
+# Number of training steps, should be a large number (>10000). Set to 100 for testing purposes
+STEPS = 100
+# Currently using JPEG images
+IMG_EXTENSION = ".jpeg"
 
 import tensorflow as tf
 
 import os
-import pathlib
 import time
 import datetime
 
@@ -36,15 +54,11 @@ def load(image_file):
 
   return input_image, real_image
 
-# Number of images in training set
-BUFFER_SIZE = 1
 # The batch size of 1 produced better results for the U-Net in the original pix2pix experiment
 BATCH_SIZE = 1
 # Each image is 256x256 in size
 IMG_WIDTH = 256
 IMG_HEIGHT = 256
-# Currently using JPEG images
-IMG_EXTENSION = ".jpeg"
 
 ######################################################
 inp, re = load('train/0' + IMG_EXTENSION)
@@ -96,6 +110,7 @@ def random_jitter(input_image, real_image):
 ######################################################
 plt.figure(figsize=(6, 6))
 for i in range(4):
+  inp, re = resize(inp, re, 256, 256)
   rj_inp, rj_re = random_jitter(inp, re)
   plt.subplot(2, 2, i + 1)
   plt.imshow(rj_inp / IMG_HEIGHT)
@@ -235,3 +250,178 @@ tf.keras.utils.plot_model(generator, show_shapes=True, dpi=64)
 #Test the generator:
 gen_output = generator(inp[tf.newaxis, ...], training=False)
 plt.imshow(gen_output[0, ...])
+
+#Define generator loss
+LAMBDA = 100
+
+loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+def generator_loss(disc_generated_output, gen_output, target):
+  gan_loss = loss_object(tf.ones_like(disc_generated_output), disc_generated_output)
+
+  # Mean absolute error
+  l1_loss = tf.reduce_mean(tf.abs(target - gen_output))
+
+  total_gen_loss = gan_loss + (LAMBDA * l1_loss)
+
+  return total_gen_loss, gan_loss, l1_loss
+
+#Build discriminator
+def Discriminator():
+  initializer = tf.random_normal_initializer(0., 0.02)
+
+  inp = tf.keras.layers.Input(shape=[256, 256, 3], name='input_image')
+  tar = tf.keras.layers.Input(shape=[256, 256, 3], name='target_image')
+
+  x = tf.keras.layers.concatenate([inp, tar])  # (batch_size, 256, 256, channels*2)
+
+  down1 = downsample(64, 4, False)(x)  # (batch_size, 128, 128, 64)
+  down2 = downsample(128, 4)(down1)  # (batch_size, 64, 64, 128)
+  down3 = downsample(256, 4)(down2)  # (batch_size, 32, 32, 256)
+
+  zero_pad1 = tf.keras.layers.ZeroPadding2D()(down3)  # (batch_size, 34, 34, 256)
+  conv = tf.keras.layers.Conv2D(512, 4, strides=1,
+                                kernel_initializer=initializer,
+                                use_bias=False)(zero_pad1)  # (batch_size, 31, 31, 512)
+
+  batchnorm1 = tf.keras.layers.BatchNormalization()(conv)
+
+  leaky_relu = tf.keras.layers.LeakyReLU()(batchnorm1)
+
+  zero_pad2 = tf.keras.layers.ZeroPadding2D()(leaky_relu)  # (batch_size, 33, 33, 512)
+
+  last = tf.keras.layers.Conv2D(1, 4, strides=1,
+                                kernel_initializer=initializer)(zero_pad2)  # (batch_size, 30, 30, 1)
+
+  return tf.keras.Model(inputs=[inp, tar], outputs=last)
+
+#Visualize discriminator model architecture:
+discriminator = Discriminator()
+tf.keras.utils.plot_model(discriminator, show_shapes=True, dpi=64)
+
+#Test discriminator
+disc_out = discriminator([inp[tf.newaxis, ...], gen_output], training=False)
+plt.imshow(disc_out[0, ..., -1], vmin=-20, vmax=20, cmap='RdBu_r')
+plt.colorbar()
+
+#Define discriminator loss
+def discriminator_loss(disc_real_output, disc_generated_output):
+  real_loss = loss_object(tf.ones_like(disc_real_output), disc_real_output)
+
+  generated_loss = loss_object(tf.zeros_like(disc_generated_output), disc_generated_output)
+
+  total_disc_loss = real_loss + generated_loss
+
+  return total_disc_loss
+
+#Define optimizers and a checkpoint-saver
+generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                 discriminator_optimizer=discriminator_optimizer,
+                                 generator=generator,
+                                 discriminator=discriminator)
+
+#Generate images
+def generate_images(model, test_input, tar):
+  prediction = model(test_input, training=True)
+  plt.figure(figsize=(15, 15))
+
+  display_list = [test_input[0], tar[0], prediction[0]]
+  title = ['Input Image', 'Ground Truth', 'Predicted Image']
+
+  for i in range(3):
+    plt.subplot(1, 3, i+1)
+    plt.title(title[i])
+    # Getting the pixel values in the [0, 1] range to plot.
+    plt.imshow(display_list[i] * 0.5 + 0.5)
+    plt.axis('off')
+  plt.show()
+  
+#Test generation
+for example_input, example_target in test_dataset.take(1):
+  generate_images(generator, example_input, example_target)
+  
+#Training
+log_dir="logs/"
+
+summary_writer = tf.summary.create_file_writer(
+  log_dir + "fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+@tf.function
+def train_step(input_image, target, step):
+  with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+    gen_output = generator(input_image, training=True)
+
+    disc_real_output = discriminator([input_image, target], training=True)
+    disc_generated_output = discriminator([input_image, gen_output], training=True)
+
+    gen_total_loss, gen_gan_loss, gen_l1_loss = generator_loss(disc_generated_output, gen_output, target)
+    disc_loss = discriminator_loss(disc_real_output, disc_generated_output)
+
+  generator_gradients = gen_tape.gradient(gen_total_loss,
+                                          generator.trainable_variables)
+  discriminator_gradients = disc_tape.gradient(disc_loss,
+                                               discriminator.trainable_variables)
+
+  generator_optimizer.apply_gradients(zip(generator_gradients,
+                                          generator.trainable_variables))
+  discriminator_optimizer.apply_gradients(zip(discriminator_gradients,
+                                              discriminator.trainable_variables))
+
+  with summary_writer.as_default():
+    tf.summary.scalar('gen_total_loss', gen_total_loss, step=step//1000)
+    tf.summary.scalar('gen_gan_loss', gen_gan_loss, step=step//1000)
+    tf.summary.scalar('gen_l1_loss', gen_l1_loss, step=step//1000)
+    tf.summary.scalar('disc_loss', disc_loss, step=step//1000)
+    
+#Training loop
+def fit(train_ds, test_ds, steps):
+  example_input, example_target = next(iter(test_ds.take(1)))
+  start = time.time()
+
+  for step, (input_image, target) in train_ds.repeat().take(steps).enumerate():
+    if (step) % 1000 == 0:
+      display.clear_output(wait=True)
+
+      if step != 0:
+        print(f'Time taken for 1000 steps: {time.time()-start:.2f} sec\n')
+
+      start = time.time()
+
+      generate_images(generator, example_input, example_target)
+      print(f"Step: {step//1000}k")
+
+    train_step(input_image, target, step)
+
+    # Training step
+    if (step+1) % 10 == 0:
+      print('.', end='', flush=True)
+
+
+    # Save (checkpoint) the model every 5k steps
+    if (step + 1) % 5000 == 0:
+      checkpoint.save(file_prefix=checkpoint_prefix)
+      
+"""
+Save logs in Tenserboard: 
+%load_ext tensorboard
+%tensorboard --logdir {log_dir}
+"""
+
+#Run training loop
+#steps should be large (>10000). 10 for testing purposes
+fit(train_dataset, test_dataset, steps=STEPS)
+
+"""
+!ls {checkpoint_dir}
+"""
+# Restoring the latest checkpoint in checkpoint_dir
+checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+
+# Run the trained model on a few examples from the test set
+for inp, tar in test_dataset.take(5):
+  generate_images(generator, inp, tar)
